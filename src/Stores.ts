e7149acfc2e4e63ad3a10b2eb90a630700008fe9
Tormak <63308171+Tormak9970@users.noreply.github.com>
2022-11-05 16:14:25 -0500

    added store

--- src/Stores.ts
+++ src/Stores.ts
@@ -4,5 +4,6 @@ import type { Writable } from "svelte/store";
 export const appDataDir = writable("");
 export const saveDirPath = writable("");
 
+export const unchangedTabs:Writable<{}> = writable({});
 export const tabs:Writable<{}> = writable({});
 export const selectedTab = writable("");
