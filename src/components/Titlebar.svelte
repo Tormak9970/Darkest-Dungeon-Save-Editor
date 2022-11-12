0696c5536982ae5684e60e6c887babf5d8bb2951
Tormak <63308171+Tormak9970@users.noreply.github.com>
2022-11-12 11:35:38 -0600

    added icons

--- src/components/Titlebar.svelte
+++ src/components/Titlebar.svelte
@@ -95,7 +95,7 @@
 
 <div data-tauri-drag-region class="titlebar">
     <div class="info">
-        <img src="/logo.svg" alt="logo" height="15" style="margin-left: 10px;">
+        <img src="/logo.png" alt="logo" height="25" style="margin-left: 7px;">
         <div style="margin-left: 10px; margin-right: 30px;">Darkest Dungeon Save Editor</div>
     </div>
     <div class="btns">
