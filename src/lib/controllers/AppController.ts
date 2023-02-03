7106b48cf8a4dfc17f45434b8ee1550a5405a77b
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-02-02 19:31:59 -0600

    testing prettier

--- src/lib/controllers/AppController.ts
+++ src/lib/controllers/AppController.ts
@@ -1,7 +1,7 @@
 /**
  * DarkestDungeon Save Editor is a tool for viewing and modifying DarkestDungeon game saves.
  * Copyright (C) 2022 Travis Lane (Tormak)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -16,8 +16,19 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>
  */
 import { fs, path } from "@tauri-apps/api";
-import { get,  } from "svelte/store";
-import { appDataDir, changedTabs, discardChangesDisabled, dsonFiles, fileNamesPath, saveChangesDisabled, saveDirPath, selectedTab, tabs, unchangedTabs } from "../../Stores";
+import { get } from "svelte/store";
+import {
+  appDataDir,
+  changedTabs,
+  discardChangesDisabled,
+  dsonFiles,
+  fileNamesPath,
+  saveChangesDisabled,
+  saveDirPath,
+  selectedTab,
+  tabs,
+  unchangedTabs,
+} from "../../Stores";
 import { DsonFile } from "../models/DsonFile";
 import { DsonTypes } from "../models/DsonTypes";
 import { DsonWriter } from "../models/DsonWriter";
@@ -32,142 +43,151 @@ import { ToasterController } from "./ToasterController";
  * The main controller for the application
  */
 export class AppController {
-    static namesController = new GenerateNamesController();
-    static backupsController = new BackupsController();
+  static namesController = new GenerateNamesController();
+  static backupsController = new BackupsController();
 
-    /**
-     * Sets up the app
-     */
-    static async init() {
-        const appDir = get(appDataDir);
-        const backupPath = await path.join(appDir, "backups");
-        // @ts-ignore
-        if (!(await fs.exists(backupPath))) await fs.createDir(backupPath);
-        this.backupsController.setBackupDir(backupPath);
-    }
+  /**
+   * Sets up the app
+   */
+  static async init() {
+    const appDir = get(appDataDir);
+    const backupPath = await path.join(appDir, "backups");
+    // @ts-ignore
+    if (!(await fs.exists(backupPath))) await fs.createDir(backupPath);
+    this.backupsController.setBackupDir(backupPath);
+  }
 
-    /**
-     * Loads the user's save files
-     */
-    static async loadSaves() {
-        const saveDir = get(saveDirPath);
+  /**
+   * Loads the user's save files
+   */
+  static async loadSaves() {
+    const saveDir = get(saveDirPath);
 
-        const newTabs = {};
-        const newDsonFiles = {};
-        const wasChanged = {};
-        if (saveDir != "") {
-            const loaderId = ToasterController.showLoaderToast("Loading save data");
-            const saveConts = await fs.readDir(saveDir);
+    const newTabs = {};
+    const newDsonFiles = {};
+    const wasChanged = {};
+    if (saveDir != "") {
+      const loaderId = ToasterController.showLoaderToast("Loading save data");
+      const saveConts = await fs.readDir(saveDir);
 
-            for (let i = 0; i < saveConts.length; i++) {
-                const saveFilePath = saveConts[i];
-                
-                if (Utils.isSaveFile(saveFilePath.name)) {
-                    const data = await fs.readBinaryFile(saveFilePath.path);
-                    const reader = new Reader(data);
-                    const dson = new DsonFile(reader, UnhashBehavior.POUNDUNHASH);
+      for (let i = 0; i < saveConts.length; i++) {
+        const saveFilePath = saveConts[i];
 
-                    newTabs[saveFilePath.name] = dson.asJson();
-                    newDsonFiles[saveFilePath.name] = dson;
-                    wasChanged[saveFilePath.name] = false;
-                }
-            }
-            ToasterController.remLoaderToast(loaderId);
-		
-            setTimeout(() => {
-                ToasterController.showSuccessToast("Saves loaded!");
-            }, 500);
+        if (Utils.isSaveFile(saveFilePath.name)) {
+          const data = await fs.readBinaryFile(saveFilePath.path);
+          const reader = new Reader(data);
+          const dson = new DsonFile(reader, UnhashBehavior.POUNDUNHASH);
+
+          newTabs[saveFilePath.name] = dson.asJson();
+          newDsonFiles[saveFilePath.name] = dson;
+          wasChanged[saveFilePath.name] = false;
         }
+      }
+      ToasterController.remLoaderToast(loaderId);
 
-        unchangedTabs.set(JSON.parse(JSON.stringify(newTabs)));
-        changedTabs.set(wasChanged);
-        tabs.set(newTabs);
-        dsonFiles.set(newDsonFiles);
-
-        discardChangesDisabled.set(true);
-        saveChangesDisabled.set(true);
+      setTimeout(() => {
+        ToasterController.showSuccessToast("Saves loaded!");
+      }, 500);
     }
 
-    /**
-     * Backs up the user's saves
-     */
-    static async backup() { await AppController.backupsController.backup(); }
+    unchangedTabs.set(JSON.parse(JSON.stringify(newTabs)));
+    changedTabs.set(wasChanged);
+    tabs.set(newTabs);
+    dsonFiles.set(newDsonFiles);
 
-    /**
-     * Load up the existing backups
-     */
-    static async loadBackups() { await AppController.backupsController.showBackupsModal(); }
+    discardChangesDisabled.set(true);
+    saveChangesDisabled.set(true);
+  }
 
-    /**
-     * Saves the current changes
-     */
-    static async saveChanges() {
-        const revision = Object.values(get(dsonFiles))[0].header.revision;
-        const changes = Object.entries(get(tabs));
-        const cTabs = get(changedTabs);
+  /**
+   * Backs up the user's saves
+   */
+  static async backup() {
+    await AppController.backupsController.backup();
+  }
 
-        // TODO only write files with changes
-        for (let i = 0; i < changes.length; i++) {
-            const fileName = changes[i][0];
-            
-            if (cTabs[fileName]) {
-                const filePath = await path.join(get(saveDirPath), fileName);
-                const newData = changes[i][1];
-                
-                const dWriter = new DsonWriter(newData as any, revision);
-                const dataBuf = dWriter.bytes();
+  /**
+   * Load up the existing backups
+   */
+  static async loadBackups() {
+    await AppController.backupsController.showBackupsModal();
+  }
 
-                await fs.writeBinaryFile(filePath, dataBuf);
-            }
-        }
+  /**
+   * Saves the current changes
+   */
+  static async saveChanges() {
+    const revision = Object.values(get(dsonFiles))[0].header.revision;
+    const changes = Object.entries(get(tabs));
+    const cTabs = get(changedTabs);
 
-        discardChangesDisabled.set(true);
-        saveChangesDisabled.set(true);
+    // TODO only write files with changes
+    for (let i = 0; i < changes.length; i++) {
+      const fileName = changes[i][0];
+
+      if (cTabs[fileName]) {
+        const filePath = await path.join(get(saveDirPath), fileName);
+        const newData = changes[i][1];
+
+        const dWriter = new DsonWriter(newData as any, revision);
+        const dataBuf = dWriter.bytes();
+
+        await fs.writeBinaryFile(filePath, dataBuf);
+      }
     }
 
-    /**
-     * Discards the current changes
-     */
-    static async discardChanges() {
-        const originalJsons = get(unchangedTabs);
-        tabs.set(JSON.parse(JSON.stringify(originalJsons)));
+    discardChangesDisabled.set(true);
+    saveChangesDisabled.set(true);
+  }
 
-        const selTab = get(selectedTab);
-        selectedTab.set("");
-        selectedTab.set(selTab);
+  /**
+   * Discards the current changes
+   */
+  static async discardChanges() {
+    const originalJsons = get(unchangedTabs);
+    tabs.set(JSON.parse(JSON.stringify(originalJsons)));
 
-        discardChangesDisabled.set(true);
-        saveChangesDisabled.set(true);
+    const selTab = get(selectedTab);
+    selectedTab.set("");
+    selectedTab.set(selTab);
 
-        ToasterController.showSuccessToast("Changes discarded!");
-    }
+    discardChangesDisabled.set(true);
+    saveChangesDisabled.set(true);
 
-    /**
-     * Reloads the user's saves
-     */
-    static async reload() { await AppController.loadSaves(); }
+    ToasterController.showSuccessToast("Changes discarded!");
+  }
 
-    /**
-     * Generates the name hashes used when loading user saves
-     * @param gamePath the gameData path
-     * @param modPath the modData path, if applicable
-     */
-    static async generateNames(gamePath:string, modPath:string): Promise<void> {
-        const fileNamesFilePath = get(fileNamesPath);
-        const names = await AppController.namesController.generateNames(gamePath, modPath);
+  /**
+   * Reloads the user's saves
+   */
+  static async reload() {
+    await AppController.loadSaves();
+  }
 
-        await fs.writeTextFile(fileNamesFilePath, Array.from(names).join('\n'));
+  /**
+   * Generates the name hashes used when loading user saves
+   * @param gamePath the gameData path
+   * @param modPath the modData path, if applicable
+   */
+  static async generateNames(gamePath: string, modPath: string): Promise<void> {
+    const fileNamesFilePath = get(fileNamesPath);
+    const names = await AppController.namesController.generateNames(
+      gamePath,
+      modPath
+    );
 
-        DsonTypes.offerNames(Array.from(names));
-    }
+    await fs.writeTextFile(fileNamesFilePath, Array.from(names).join("\n"));
 
-    /**
-     * Updates the in-memory hash names based on a cached file
-     */
-    static async updateNames(): Promise<void> {
-        const fileNamesFilePath = get(fileNamesPath);
-        const names = (await fs.readTextFile(fileNamesFilePath)).split('\n');
+    DsonTypes.offerNames(Array.from(names));
+  }
 
-        DsonTypes.offerNames(Array.from(names));
-    }
+  /**
+   * Updates the in-memory hash names based on a cached file
+   */
+  static async updateNames(): Promise<void> {
+    const fileNamesFilePath = get(fileNamesPath);
+    const names = (await fs.readTextFile(fileNamesFilePath)).split("\n");
+
+    DsonTypes.offerNames(Array.from(names));
+  }
 }
